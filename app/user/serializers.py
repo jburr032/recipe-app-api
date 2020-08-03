2d133426e2b55f470e000a9de46e70e60fd2f01b
Josh Burrill <joshua.burrill.1@gmail.com>
2020-08-03 14:04:40 +0100

    Added manage user endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model, authenticate
 
 
 class UserSerializer(serializers.ModelSerializer):
-
+    """ Serializer for the user object """
     class Meta:
         model = get_user_model()
         fields = ('email', 'password', 'name')
@@ -18,8 +18,20 @@ class UserSerializer(serializers.ModelSerializer):
         """ Create a new user with an encrypted password and return it """
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class TokenSerializer(serializers.Serializer):
+    """ Serializer for the user authentication object """
     email = serializers.CharField()
     password = serializers.CharField(
         style={'input_type': 'password'},
--- app/user/views.py
+++ app/user/views.py
@@ -1,5 +1,5 @@
 from .serializers import UserSerializer, TokenSerializer
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -13,3 +13,13 @@ class CreateTokenView(ObtainAuthToken):
     """ Create a new auth token for a user """
     serializer_class = TokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
